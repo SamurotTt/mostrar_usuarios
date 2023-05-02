03d10e505da3b90d7ece8ac7320577b3cbeff1c3
SamurotTt (埃里克) <96399829+SamurotTt@users.noreply.github.com>
2023-05-02 19:26:33 -0300

    Update AdicionarUsuarios.py

--- AdicionarUsuarios.py
+++ AdicionarUsuarios.py
@@ -3,9 +3,9 @@
 import subprocess
 
 #definindo uma variável para usuário e senha
-novo_usuário = "Erico_limdo"
+novo_usuario = "Erico_limdo"
 nova_senha = "banana1223"
 
 #cria o usuário e senha no sistema Linux
-subprocess.run(["sudo", "useradd", novo_usuário])
-subprocess.run(["sudo", "passwd", novo_usuário], input=f"{nova_senha}\n{nova_senha}\n".encode())
+subprocess.run(["sudo", "useradd", novo_usuario])
+subprocess.run(["sudo", "passwd", novo_usuario], input=f"{nova_senha}\n{nova_senha}\n".encode())
