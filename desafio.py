bf9334a20b939d1127e49284ce560716c12a5e48
SamurotTt (埃里克) <96399829+SamurotTt@users.noreply.github.com>
2023-05-02 19:28:18 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -2,11 +2,11 @@
 
 import pwd, subprocess
 
-novo_usuário = "Eric"
+novo_usuario = "Eric"
 nova_senha = "banana1223"
 
-subprocess.run(["sudo", "useradd", novo_usuário])
-subprocess.run(["sudo", "passwd", novo_usuário], input=f"{nova_senha}\n{nova_senha}\n".encode())
+subprocess.run(["sudo", "useradd", novo_usuario])
+subprocess.run(["sudo", "passwd", novo_usuario], input=f"{nova_senha}\n{nova_senha}\n".encode())
 
 for usuarios in pwd.getpwall():
     print(usuarios.pw_name)
